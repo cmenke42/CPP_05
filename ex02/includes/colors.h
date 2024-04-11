8b12cd119b9f214185f8256c9a22573bd8375d3c
Carlos Menke <carlos.menke@icloud.com>
2024-04-11 16:48:05 +0000

    update ex02

---
+++ ex02/includes/colors.h
@@ -0,0 +1,8 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define BLUE "\033[1;34m"
+#define RED "\033[1;31m"
+#define RESET "\033[0m"
+
+#endif /* COLORS_H */
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,24 +1,63 @@
-#include "Bureaucrat.hpp"
-
 #include <iostream>
 
+#include "Bureaucrat.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
+#include "colors.h"
 
 int main(void)
 {
-	Bureaucrat	max("max", 6);
-	// ShrubberyCreationForm tree("testing");
+	Bureaucrat	max("max", 1);
+	Bureaucrat	min("min", 150);
 
-	AForm* tree = new PresidentialPardonForm("testing");
+	ShrubberyCreationForm tree("tree");
+	RobotomyRequestForm robot("robot");
+	PresidentialPardonForm president("president");
 
-	max.signForm(*tree);
-	max.executeForm(*tree);
-	
-	// AForm		buyComputer("buyComputer", 49, 60);
+	std::cout << RED << tree << RESET << std::endl;
+	std::cout << RED << robot << RESET << std::endl;
+	std::cout << RED << president << RESET << std::endl;
+	std::cout << std::endl;
 
-	// max.signForm(buyComputer);
+	std::cout << BLUE << max << RESET << "\n";
+	max.signForm(tree);
+	max.signForm(robot);
+	max.signForm(president);
+	std::cout << std::endl;
 
-	// std::cout << buyComputer << std::endl;
+	std::cout << RED << tree << RESET << std::endl;
+	std::cout << RED << robot << RESET << std::endl;
+	std::cout << RED << president << RESET << std::endl;
+	std::cout << std::endl;
+
+	max.executeForm(tree);
+	std::cout << std::endl;
+	max.executeForm(robot);
+	std::cout << std::endl;
+	max.executeForm(president);
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+// ---------------------
+	std::cout << BLUE << min << RESET << "\n";
+	min.signForm(tree);
+	min.signForm(robot);
+	min.signForm(president);
+	std::cout << std::endl;
+
+	min.executeForm(tree);
+	min.executeForm(robot);
+	min.executeForm(president);
+	std::cout << std::endl;
+
+// ------------------------
+
+	AForm* treePtr = new PresidentialPardonForm("testing");
+
+	std::cout << RED << *treePtr << RESET << std::endl;
+
+	max.executeForm(*treePtr);
+
+	delete treePtr;
 }
